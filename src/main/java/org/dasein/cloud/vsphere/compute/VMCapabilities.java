e97b864189f8b42d90e569f8a756e31d4a20d118
Stas Maksimov <maksimov@gmail.com>
2015-03-25 23:28:57 +0000

    Add supportsClientRequestToken() capability (false)

--- src/main/java/org/dasein/cloud/vsphere/compute/VMCapabilities.java
+++ src/main/java/org/dasein/cloud/vsphere/compute/VMCapabilities.java
@@ -237,6 +237,11 @@ public class VMCapabilities extends AbstractCapabilities<PrivateCloud> implement
     }
 
     @Override
+    public boolean supportsClientRequestToken() throws InternalException, CloudException {
+        return false;
+    }
+
+    @Override
     public boolean supportsAlterVM() {
         return true;
     }
